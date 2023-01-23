e682c03705e45d7199f5f9ec65624a1b634d0c54
huang changqiang <hcq0618@163.com>
2023-01-23 22:14:12 +0800

    Add video upload api

--- twitter-core/src/main/java/com/twitter/sdk/android/core/services/MediaService.kt
+++ twitter-core/src/main/java/com/twitter/sdk/android/core/services/MediaService.kt
@@ -18,6 +18,7 @@ package com.twitter.sdk.android.core.services
 
 import com.twitter.sdk.android.core.models.*
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -40,5 +41,42 @@ interface MediaService {
         @Part("media") media: RequestBody?,
         @Part("media_data") mediaData: RequestBody?,
         @Part("additional_owners") additionalOwners: RequestBody?
+    ): Call<Media>
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload-init
+    @FormUrlEncoded
+    @POST("https://upload.twitter.com/1.1/media/upload.json")
+    fun init(
+        @Field("command") command: String = "INIT",
+        @Field("media_type") mediaType: String,
+        @Field("total_bytes") totalBytes: Long,
+        @Field("media_category") mediaCategory: String? = "tweet_video",
+        @Field("additional_owners") additionalOwners: RequestBody? = null
+    ): Call<Media>
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload-append
+    @Multipart
+    @POST("https://upload.twitter.com/1.1/media/upload.json")
+    fun append(
+        @Part("command") command: RequestBody = "APPEND".toRequestBody(),
+        @Part("media_id") mediaId: RequestBody,
+        @Part("media") media: RequestBody? = null,
+        @Part("media_data") mediaData: RequestBody? = null,
+        @Part("segment_index") segmentIndex: RequestBody
+    ): Call<Unit>
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload-finalize
+    @FormUrlEncoded
+    @POST("https://upload.twitter.com/1.1/media/upload.json")
+    fun finalize(
+        @Field("command") command: String = "FINALIZE",
+        @Field("media_id") mediaId: Long
+    ): Call<Media>
+
+    // https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/get-media-upload-status
+    @GET("https://upload.twitter.com/1.1/media/upload.json")
+    fun checkStatus(
+        @Query("command") command: String = "STATUS",
+        @Query("media_id") mediaId: Long
     ): Call<Media>
 }
